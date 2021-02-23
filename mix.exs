9d59d2201fc681e90e53376a4a375053642f2e45
ericsalim <ric_sal@yahoo.com>
2021-02-23 21:02:05 +0700

    Increment to version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exemvi.MixProject do
   def project do
     [
       app: :exemvi,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       name: "Exemvi",
       description: description(),
