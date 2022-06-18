6baebce8ea24ddbc2479b0c627fa3899311dd424
ericsalim <ric_sal@yahoo.com>
2022-06-18 11:52:36 +0700

    Update Exemvi version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exemvi.MixProject do
   def project do
     [
       app: :exemvi,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.11",
       name: "Exemvi",
       description: description(),
